954e1bfb9e3f649378dfb1847511081c31275149
MarkoM10 <58051779+MarkoM10@users.noreply.github.com>
2023-06-11 14:20:31 +0200

    Hero section code.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import "../src/styles/App.scss";
+import logoSvg from "../src/images/logo.svg";
+import downArrow from "../src/images/icon-arrow-down.svg";
 
 function App() {
   return (
-    <div className="pageWrapper">
-      <header>
-        <nav className="navbar"></nav>
-      </header>
+    <div className="siteWrapper">
+      <section className="heroSection">
+        <div className="headerWrapper">
+          <header>
+            <a href="#">
+              {" "}
+              <img src={logoSvg}></img>
+            </a>
+            <nav className="navbar">
+              <ul>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Projects</a>
+                </li>
+                <li>
+                  <a href="#">CONTACT</a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+        </div>
+        <div className="heroHeadingWrapper">
+          <h1>WE ARE CREATIVES</h1>
+          <img src={downArrow}></img>
+        </div>
+      </section>
     </div>
   );
 }
