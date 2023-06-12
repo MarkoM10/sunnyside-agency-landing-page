d8bd2ef4084b207a408c4daf90d37ccd0f9da3df
MarkoM10 <58051779+MarkoM10@users.noreply.github.com>
2023-06-12 22:55:04 +0200

    Articles code

--- src/App.js
+++ src/App.js
@@ -35,6 +35,36 @@ function App() {
           <img src={downArrow}></img>
         </div>
       </section>
+      <section className="articles">
+        <section className="firstArticle">
+          <div className="leftSide">
+            <div>
+              <label>Transform your brand</label>
+              <p>
+                We are a full-service creative agency specializing in helping
+                brands grow fast. Engage your clients through compelling visuals
+                that do most of the marketing for you.
+              </p>
+              <a href="#">LEARN MORE</a>
+            </div>
+          </div>
+          <div></div>
+        </section>
+        <section className="secondArticle">
+          <div className="leftImage"></div>
+          <div className="rightSide">
+            <div>
+              <label>Stand out to the right audience</label>
+              <p>
+                Using a collaborative formula of designers, researchers,
+                photographers, videographers, and copywriters, we'll build and
+                extend your brand in digital places.
+              </p>
+              <a href="#">LEARN MORE</a>
+            </div>
+          </div>
+        </section>
+      </section>
     </div>
   );
 }
